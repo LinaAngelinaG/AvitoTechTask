28272a763fbc347047d74fbf289510b230cce2c7
gusevang <8207042@gmail.com>
2023-08-27 22:32:12 +0300

    some storage options added

--- internal/userinsegment/storage.go
+++ internal/userinsegment/storage.go
@@ -12,17 +12,23 @@ type Repository interface {
 }
 
 //GetSegments::
-//SELECT segment_name FROM (SELECT segment_id AS segment_id FROM userinsegment
+//SELECT segment_name FROM (SELECT segment_id AS segment_id FROM user_in_segment
 //WHERE user_id = $1 && out_date IS NULL OR out_date < current_date) AS segments
 //INNER JOIN segment ON segments.segment_id = segment.segment_id
 //
 //AddSegments::
-//INSERT INTO segment(segment_id, segment_name, active) VALUES (DEFAULT, $1, DEFAULT)
+//INSERT INTO user_in_segment(user_id, segment_id, in_date, out_date)
+//VALUES ($1, (SElECT segment_id FROM segment WHERE segment_name = $2), current_timestamp, DEFAULT)
+//
+//INSERT INTO user_in_segment(user_id, segment_id, in_date, out_date)
+//VALUES ($1, (SElECT segment_id FROM segment WHERE segment_name = $2), current_timestamp, $3)
 //
 //DeleteSegments::
-// UPDATE userinsegment SET (out_date) = current_timestamp WHERE segment_id =
+// UPDATE user_in_segment SET (out_date) = current_timestamp WHERE segment_id =
 //(SELECT segment_id FROM segment WHERE segment_name = $1) AND user_id = $2
 //
 //UPDATE segment SET (active) =
 // (SELECT active FROM segment WHERE segment_name = $1)
 //
+//Get history for user:
+//SELECT * from user_in_segment WHERE user_id = $1
